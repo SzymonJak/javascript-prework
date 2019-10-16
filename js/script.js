b8183a9e49ba382c9f7a98a9c1fdffd7d2e7cf08
Szymon Jakubiak <jakubiak.sz@gmail.com>
2019-10-16 17:25:32 +0200

    Create game interface with buttons

--- js/script.js
+++ js/script.js
@@ -1,54 +1,73 @@
-function getMoveName(argMoveId) {
-    if (argMoveId == 1) {
-        return 'ROCK';
-    } else if (argMoveId == 2) {
-        return 'PAPER';
-    } else if (argMoveId == 3) {
-        return 'SCISSORS';
-    } else {
-        printMessage('I don\'t the move by the id of ' + argMoveId + '.');
-        return 'unknown move';
+function playGame(playerInput) {
+
+    clearMessages();
+
+    function getMoveName(argMoveId) {
+        if (argMoveId == 1) {
+            return 'ROCK';
+        } else if (argMoveId == 2) {
+            return 'PAPER';
+        } else if (argMoveId == 3) {
+            return 'SCISSORS';
+        } else {
+            printMessage('I don\'t the move by the id of ' + argMoveId + '.');
+            return 'unknown move';
+        }
     }
+
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    let argComputerMove = getMoveName(randomNumber);
+
+    console.log('Random number is: ' + randomNumber);
+
+    printMessage('My move is: ' + argComputerMove);
+
+    // let playerInput = prompt('Pick you move! 1: ROCK, 2: PAPER, 3: SCISSORS');
+
+    let argPlayerMove = getMoveName(playerInput);
+
+    console.log('Player input: ' + playerInput);
+
+    printMessage('Your move is: ' + argPlayerMove);
+
+    function displayResult(argComputerMove, argPlayerMove) {
+        console.log('move: ', argComputerMove, argPlayerMove);
+        if (argComputerMove == 'ROCK' && argPlayerMove == 'PAPER') {
+            printMessage('YOU win!');
+        } else if (argComputerMove == 'ROCK' && argPlayerMove == 'SCISSORS') {
+            printMessage('I win!');
+        } else if (argComputerMove == 'PAPER' && argPlayerMove == 'ROCK') {
+            printMessage('I win!');
+        } else if (argComputerMove == 'PAPER' && argPlayerMove == 'SCISSORS') {
+            printMessage('YOU win!');
+        } else if (argComputerMove == 'SCISSORS' && argPlayerMove == 'ROCK') {
+            printMessage('YOU win!');
+        } else if (argComputerMove == 'SCISSORS' && argPlayerMove == 'PAPER') {
+            printMessage('I win!');
+        } else if (argComputerMove == argPlayerMove) {
+            printMessage('DRAW!');
+        } else {
+            printMessage('you picked WRONG number');
+        }
+    }
+
+    displayResult(argComputerMove, argPlayerMove);
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+let rockButton = document.getElementById('play-rock');
+let paperButton = document.getElementById('play-paper');
+let scissorsButton = document.getElementById('play-scissors');
 
-let argComputerMove = getMoveName(randomNumber);
-
-console.log('Random number is: ' + randomNumber);
-
-printMessage('My move is: ' + argComputerMove);
-
-let playerInput = prompt('Pick you move! 1: ROCK, 2: PAPER, 3: SCISSORS');
-
-let argPlayerMove = getMoveName(playerInput);
-
-console.log('Player input: ' + playerInput);
-
-printMessage('Your move is: ' + argPlayerMove);
-
-function displayResult(argComputerMove, argPlayerMove) {
-    console.log('move: ', argComputerMove, argPlayerMove);
-    if (argComputerMove == 'ROCK' && argPlayerMove == 'PAPER') {
-        printMessage('YOU win!');
-    } else if (argComputerMove == 'ROCK' && argPlayerMove == 'SCISSORS') {
-        printMessage('I win!');
-    } else if (argComputerMove == 'PAPER' && argPlayerMove == 'ROCK') {
-        printMessage('I win!');
-    } else if (argComputerMove == 'PAPER' && argPlayerMove == 'SCISSORS') {
-        printMessage('YOU win!');
-    } else if (argComputerMove == 'SCISSORS' && argPlayerMove == 'ROCK') {
-        printMessage('YOU win!');
-    } else if (argComputerMove == 'SCISSORS' && argPlayerMove == 'PAPER') {
-        printMessage('I win!');
-    } else if (argComputerMove == argPlayerMove) {
-        printMessage('DRAW!');
-    } else {
-        printMessage('you picked WRONG number');
-    }
-}
-
-displayResult(argComputerMove, argPlayerMove);
+rockButton.addEventListener('click', function() {
+    playGame(1);
+});
+paperButton.addEventListener('click', function() {
+    playGame(2);
+});
+scissorsButton.addEventListener('click', function() {
+    playGame(3);
+});
 
 //  Step By Step
 
